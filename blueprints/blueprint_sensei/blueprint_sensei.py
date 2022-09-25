abee9a1dd2dee64c91df5ed4b891e41c17b992b4
Navdeep Sharma <navdeepisno1@gmail.com>
2022-09-25 23:29:47 +0530

    mm

---
+++ blueprints/blueprint_sensei/blueprint_sensei.py
@@ -0,0 +1,19 @@
+from dependencies import *
+
+blueprint_sensei = Blueprint('blueprint_sensei', __name__)
+
+@blueprint_sensei.route('/sensei-all')
+def sensei_all():
+    responseTemplate["app-theme"] = request.cookies.get('app-theme')
+    responseTemplate["countries"] = COUNTRIES
+    return render_template('sensei-all/sensei-all.htm', res=responseTemplate)
+
+
+@blueprint_sensei.route('/sensei/<username>')
+def sensei_user(username):
+    responseTemplate["app-theme"] = request.cookies.get('app-theme')
+    responseTemplate["countries"] = COUNTRIES
+    responseTemplate["user"] = {
+        "username": username
+    }
+    return render_template('sensei-profile/sensei-profile.htm', res=responseTemplate)
--- flask_app.py
+++ flask_app.py
@@ -1,23 +1,10 @@
 from dependencies import *
 from blueprints_all import *
 
-
-@app.route("/trending-dishes-all")
-def trending_skills_all():
-    responseTemplate["app-theme"] = request.cookies.get('app-theme')
-    return render_template("trending-dishes-all/trending-dishes-all.htm", res=responseTemplate)
-
-
-@app.route("/trending-shops-all")
-def trending_shops_all():
-    responseTemplate["app-theme"] = request.cookies.get('app-theme')
-    return render_template("trending-shops-all/trending-shops-all.htm", res=responseTemplate)
-
-
-@app.route("/menu")
-def menu():
-    responseTemplate["app-theme"] = request.cookies.get('app-theme')
-    return render_template("menu/menu.htm", res=responseTemplate)
+app.register_blueprint(blueprint_user, url_prefix="/user")
+app.register_blueprint(blueprint_utilities, url_prefix="")
+app.register_blueprint(api_places, url_prefix=PATH_API+"places")
+app.register_blueprint(blueprint_sensei, url_prefix="")
 
 
 @app.errorhandler(404)
@@ -26,9 +13,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.register_blueprint(blueprint_user, url_prefix="/user")
-    app.register_blueprint(blueprint_utilities, url_prefix="")
-    app.register_blueprint(api_places, url_prefix=PATH_API+"places")
-    app.register_blueprint(blueprint_sensei_all)
-
     app.run(debug=True)
